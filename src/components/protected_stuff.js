f7efeee089ae5f49a108b94a23c04dbee3bf3be1
jay <jamesjayko@gmail.com>
2018-01-15 17:36:15 -0800

    finished auth

---
+++ src/components/protected_stuff.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default props => (
+  <div>
+    <h2 className="align-center">Protected Stuff</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+      numquam sequi, voluptate distinctio totam deserunt non harum, soluta
+      corporis inventore beatae voluptatum accusamus enim doloremque vel rerum
+      sapiente itaque veritatis.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+      numquam sequi, voluptate distinctio totam deserunt non harum, soluta
+      corporis inventore beatae voluptatum accusamus enim doloremque vel rerum
+      sapiente itaque veritatis.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+      numquam sequi, voluptate distinctio totam deserunt non harum, soluta
+      corporis inventore beatae voluptatum accusamus enim doloremque vel rerum
+      sapiente itaque veritatis.
+    </p>
+  </div>
+);
